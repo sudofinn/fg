677c754f98bcd1c7cf2c44b521c1c403d6104ecf
Finn Guha <finnguha@Finns-MBP.fritz.box>
2024-09-17 21:19:56 +0200

    Added favicon and meta

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+import Image from "next/image"
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Finn Guha</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -42,6 +44,7 @@ export default function Home() {
               YOU WILL HEAR MY NAME AGAIN
             </p>
         </div>
+
 
         {/* 
 
